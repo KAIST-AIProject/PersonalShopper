22f948f7fd85020b40d32fbd0544bd3a63f06cf5
leem-saebom <springofbird@gmail.com>
2024-04-21 01:42:21 +0900

    review scrapper updated

---
+++ load_pickle.py
@@ -0,0 +1,8 @@
+import pickle 
+
+save_path_quality = "Naver_item1_quality.bin"
+with open(save_path_quality, 'rb') as quality_file:
+    contents = pickle.load(quality_file)
+print(contents)
+
+print(len(contents['리뷰']))
