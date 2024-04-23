e3e6aa4f29da9205d8b1ffe6340b488e61c0f75a
Chaewon829 <lcwon9@gmail.com>
2024-04-23 16:54:44 +0900

    add config_template.py

---
+++ config_template.py
@@ -0,0 +1,14 @@
+api_key = ""
+
+#Naver
+naver_id = ""
+naver_pw = ""
+#Market Kurly
+kurly_id = ""
+kurly_pw = ""
+#Gmarket
+gmarket_id = ""
+gmarket_pw = ""
+#11st
+eleventh_id = ""
+eleventh_pw = ""
