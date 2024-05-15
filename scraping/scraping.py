61e5447163dd05f96fbc96165e35af4a56d1c58e
leem-saebom <springofbird@gmail.com>
2024-05-15 16:42:52 +0900

    coupang connect without option

--- scraping/scraping.py
+++ scraping/scraping.py
@@ -12,7 +12,7 @@ from selenium.webdriver.chrome.options import Options
 from .naver_item_scrapper import *
 from .kurly_item_scrapper import *
 from .gmarket_item_scrapper import *
-
+from .coupang_item_scrapper import *
 from utils import *
 from agent import *
 import os
@@ -38,6 +38,58 @@ def CoupangLinkGet(url_kword, n_top=10,):
     for i in range(n_top):
         coupang_ntop_url.append(root+qurey_arr[i]['href'])
     return coupang_ntop_url
+
+################컬리 direct link에서 정보 불러오기################
+def CoupangFinalUrl(keyword, n_top, debug_mode=True):
+
+    # 웹드라이버 옵션 생성
+    options = webdriver.ChromeOptions()
+    count = 0
+    # 창 숨기는 옵션 추가
+    # options.add_argument("headless")
+    if debug_mode:
+        options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+    
+    driver = webdriver.Chrome(options=options)
+    url_list = CoupangLinkGet(keyword,driver, n_top)
+    coupang_url_lst = []
+
+    data_details = []
+    data_reviews = []
+    images_urls = []
+
+
+    with tqdm(total=n_top, desc='Coupang', ascii=True) as pbar:
+        for url in url_list:
+            driver.get(url)
+            option_info = {'options':dict()}
+            # CoupangOptionGet(driver, option_info)
+            scrapped_data_path = os.path.join("database", "Coupang_item_"+str(count+1)+".bin")
+            review_data_path = os.path.join("database", "Coupang_item_review_"+str(count+1)+".bin")
+            result_detail, result_review, result_image_url = Coupang_selenium_scraper(driver, scrapped_data_path, review_data_path)
+            result_detail.update(option_info)
+            count+=1
+            
+            #local로 이미지 다운로드
+            local_image_url= image_for_gpt(4, result_image_url, "database")
+            print(local_image_url)
+
+            vision_info = local_vision_gpt(local_image_url)
+            print(f"vision_info = {vision_info}")
+            result_detail['product detail form images'] = vision_info
+            
+            #compare_information : compare agent에게 제공할 정보 : 이름, 가격, 할인율, 번호, 리뷰 평균 점수...
+            compare_information = {"Product_name" : result_detail["상품명"], "discount_rate" : result_detail["할인율"], "price" : result_detail["현재 가격"], 'number of reviews' : result_review['리뷰 수'], "Star rating" : result_review['총 평점'], "reviews" : result_review['리뷰'] }
+                
+            data_details.append(result_detail)
+            data_reviews.append(compare_information)
+            images_urls.append(result_image_url)
+            coupang_url_lst.append(url)
+            count+=1
+            pbar.update(1)
+
+    driver.quit()
+    return coupang_url_lst, data_details, data_reviews
 
 ################네이버 JSON으로 상품정보 불러오기################
 def NaverLinkGet(keyword, driver, n_top=10, debug_mode=False):
@@ -261,7 +313,7 @@ def GmarketLinkGet(url_kword, n_top=10):
 
 
 
-################컬리 direct link에서 정보 불러오기################
+################Gmarket link에서 정보 불러오기################
 def GmarketFinalUrl(keyword, n_top, debug_mode=True):
 
     # 웹드라이버 옵션 생성
